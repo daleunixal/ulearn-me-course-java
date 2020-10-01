0835a5b893ad0bb12a6a79f2336c48486810b881
Nikita Zerniukov <Nikita.Zerniukov@urfu.me>
2020-10-02 01:39:18 +0500

    Зернюков Никита. Задача 3.8

--- 03-java-control-flow-and-arrays/task08/src/com.example.task08/Task08Main.java
+++ 03-java-control-flow-and-arrays/task08/src/com.example.task08/Task08Main.java
@@ -4,18 +4,11 @@ import java.util.Arrays;
 
 public class Task08Main {
     public static void main(String[] args) {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
-        int[] arr = new int[2];
-        arr[0] = 2;
-        arr[1] = 3;
-        System.out.println(mult(arr));
-         */
+       
     }
 
     static long mult(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+       
         return Arrays.stream(arr).asLongStream().reduce((x, y) -> x*y).orElse(0);
     }
 
