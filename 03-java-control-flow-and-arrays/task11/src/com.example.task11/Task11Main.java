b2adcd7fc6708a7bd5ba76fa1c5cf64d1256d3c5
Nikita Zerniukov <Nikita.Zerniukov@urfu.me>
2020-10-02 01:42:49 +0500

    Зернюков Никита. Задача 3.11

--- 03-java-control-flow-and-arrays/task11/src/com.example.task11/Task11Main.java
+++ 03-java-control-flow-and-arrays/task11/src/com.example.task11/Task11Main.java
@@ -2,13 +2,7 @@ package com.example.task11;
 
 public class Task11Main {
     public static void main(String[] args) {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
-        int[] arr = {7, 5, 9};
-        swap(arr);
-        System.out.println(java.util.Arrays.toString(arr));
-         */
+        
     }
 
     static void swap(int[] arr) {
@@ -17,7 +11,7 @@ public class Task11Main {
         int indexToSwitch = numMin(arr);
         arr[0] = arr[indexToSwitch];
         arr[indexToSwitch] = temp;
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        
     }
 
     static int numMin(int[] arr) {
