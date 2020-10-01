9e6e49da1f9de9fbf085d99125a62915da9bcadc
Nikita Zerniukov <Nikita.Zerniukov@urfu.me>
2020-10-02 01:43:34 +0500

    Зернюков Никита. Задача 3.12

--- 03-java-control-flow-and-arrays/task12/src/com.example.task12/Task12Main.java
+++ 03-java-control-flow-and-arrays/task12/src/com.example.task12/Task12Main.java
@@ -2,13 +2,7 @@ package com.example.task12;
 
 public class Task12Main {
     public static void main(String[] args) {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
-        int[] arr = {9, 11, 7, 8};
-        selectionSort(arr);
-        System.out.println(java.util.Arrays.toString(arr));
-         */
+        
     }
 
     static void selectionSort(int[] arr) {
@@ -17,7 +11,6 @@ public class Task12Main {
             swap(arr, i);
         }
     }
-
 
     static void swap(int[] arr, int firstInd) {
         int temp = arr[firstInd];
